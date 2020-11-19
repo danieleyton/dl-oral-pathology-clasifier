38901b711ab5472894f7f095bc6a96d0b2eb092e
danieleyton <daniel.labra@usach.cl>
2020-11-19 20:45:42 -0300

    entrenamiento con distintas epochs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dl-oral-pathology-clasifier
+
+![Resultados con 10 epochs](./10_epoch.png)
+
+![Resultados con 30 epochs](./30_epoch.png)
+
+![Resultados con 90 epochs](./90_epoch.png)
+
+![Resultados con 100 epochs](/.300_epochs)
--- model.py
+++ model.py
@@ -72,14 +72,14 @@ model.compile(optimizer='adam',
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'])
 
-history = model.fit(train_ds, epochs=10,
+history = model.fit(train_ds, epochs=300,
         validation_data=(val_ds))
 
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label='val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
+plt.ylim([0, 1])
 plt.legend(loc='lower right')
 plt.show()
 
